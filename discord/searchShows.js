e1d97b99013472c3d24d8d7057ffcacd75b2a847
dahazeyninja <dahazeyniinja@gmail.com>
2020-01-23 21:58:28 -0500

    add anilist stuff search both senpai and anilist titles use anilist data fof more embed info

--- discord/searchShows.js
+++ discord/searchShows.js
@@ -4,105 +4,164 @@ module.exports = async function(message, database){
 	db = database;
 	const keywords = message.content.split(' ');
 	if (keywords.length === 1) return console.log('no search keywords specified');
-	let values = [`%${keywords[1].toLowerCase()}%`];
-	let like_string = 'name_lower LIKE ?';
 	
-	for (let i = 2; i < keywords.length; i++){
+	searchShows(keywords, message);
+};
+
+function searchShows(keywords, message){
+	let values = [`%${keywords[1].toLowerCase()}%`];
+	let like_string = 'senpai.name LIKE ?';
+	let e_string = 'anilist.title_e LIKE ?';
+	let r_string = 'anilist.title_r LIKE ?';
+	let n_string = 'anilist.title_n LIKE ?';
+	
+	
+	for (let i = 2; i < keywords.length; i++) {
 		values.push(`%${keywords[i].toLowerCase()}%`);
-		like_string += ' AND name_lower LIKE ?';
+		like_string += ' AND senpai.name LIKE ?';
+		e_string += ' AND title_e LIKE ?';
+		r_string += ' AND title_r LIKE ?';
+		n_string += ' AND title_n LIKE ?';
 	}
 
-	db.get(`SELECT * FROM shows WHERE ${like_string}`, values, (err, row)=>{
-		if(err) return console.error(err, `SELECT * FROM shows WHERE ${like_string};`, values);
+	const query_string = `
+	SELECT
+		senpai.name,
+		senpai.MALID,
+		senpai.ANNID,
+		senpai.ALID,
+		senpai.season,
+		senpai.simulcast,
+		senpai.simulcast_delay,
+		senpai.airdate_u,
+		anilist.id,
+		anilist.title_e,
+		anilist.title_n,
+		anilist.title_r,
+		anilist.description,
+		anilist.startDate,
+		anilist.endDate,
+		anilist.episodes,
+		anilist.nextAiringAt,
+		anilist.coverImage,
+		anilist.genres,
+		anilist.tags
+	FROM 
+		senpai
+	LEFT JOIN anilist ON
+		senpai.ALID = anilist.id
+	WHERE (${like_string}) OR (${e_string}) OR (${r_string}) OR (${n_string});`;
 
-		if(!row){
+	const values_all = values.concat(values).concat(values).concat(values);
+	
+	db.get(query_string, values_all, (err, row) => {
+		if (err)
+			return console.error(err, query_string, values);
+		if (!row) {
 			return message.channel.send('No results found');
 		}
 
-		// console.log(row);
-		const fields = [];
-		let delay = 'No delay';
-
-		if(row.MALID){
-			fields.push({
-				name: 'MAL Link',
-				value: `https://myanimelist.net/anime/${row.MALID}`,
-				inline: false
-			});
+		if(row.id){
+			createEmbed(row);
+		} else {
+			createNoAnilistEmbed(row);
 		}
-		if(row.ANNID){
-			fields.push({
-				name: 'ANN Link',
-				value: `https://www.animenewsnetwork.com/encyclopedia/anime.php?id=${row.ANNID}`,
-				inline: false
-			});
-		}
-		if(row.simulcast_delay != 0){
-			delay = `${row.simulcast_delay}h`;
-		}
-		if(row.simulcast != 0){
-			fields.push({
-				name: 'Simulcast',
-				value: `${row.simulcast} (${delay})`,
-				inline: true
-			});
-		}
-
-		const start_date = new Date(row.airdate_u * 1000);
-		fields.push({
-			name: 'Start Date',
-			value: `${start_date.toDateString()}`,
-			inline: true
-		});
-		fields.push({
-			name: 'Season',
-			value: `${row.season}`,
-			inline: true
-		});
-
-		getNextEpisode(row);
-
-
-		const richEmbed = {
-			color: 0xaa98ae,
-			title: row.name,
-			fields,
-			timestamp: Date.now()
-		};
-
-		message.channel.send({embed:richEmbed});
 	});
-};
+}
 
-function getNextEpisode(row){
-	db.all('SELECT * FROM episodes WHERE name LIKE ?;', `%${row.name}%`, (err, rows)=>{
-		if(err) return console.error(err);
+async function createEmbed(row){
+	const fields = [];
+	const nextAiring = JSON.parse(row.nextAiringAt);
+	const tags = JSON.parse(row.tags);
+	const genres = JSON.parse(row.genres);
+	let delay = 'No delay';
 
-		const now = Date.now();
-
-		if(rows.length == 0){
-			console.log('no episodes found');
-			console.log(row.airdate_u);
-			let start_date = row.airdate_u * 1000;
-			console.log(start_date);
-			let add = 604800000;
-			let found;
-			for(let i = 0; i < 100; i++){
-				if (start_date + add > now){
-					const date = new Date(start_date + add);
-					return console.log(date.toDateString());
-				} else {
-					start_date += 604800000;
-				}
-			}
-		}
-
-		rows.forEach((episode)=>{
-			const date = new Date(episode.time * 1000);
-			if(date > now){
-				console.log(date.toDateString());
-			}
-			
+	if(row.MALID){
+		fields.push({
+			name: 'MAL Link',
+			value: `https://myanimelist.net/anime/${row.MALID}`,
+			inline: false
 		});
+	}
+	if(row.ANNID){
+		fields.push({
+			name: 'ANN Link',
+			value: `https://www.animenewsnetwork.com/encyclopedia/anime.php?id=${row.ANNID}`,
+			inline: false
+		});
+	}
+
+	if(row.ALID > 1){
+		fields.push({
+			name: 'AniList Link',
+			value: `https://anilist.co/anime/${row.ALID}`,
+			inline: false
+		});
+	}
+
+	if(row.simulcast_delay != 0){
+		delay = `${row.simulcast_delay}h`;
+	}
+	if(row.simulcast != 0){
+		fields.push({
+			name: 'Simulcast',
+			value: `${row.simulcast} (${delay})`,
+			inline: true
+		});
+	}
+
+	const start_date = new Date(row.airdate_u * 1000);
+	fields.push({
+		name: 'Start Date',
+		value: `${start_date.toDateString()}`,
+		inline: true
+	});
+	fields.push({
+		name: 'Season',
+		value: `${row.season}`,
+		inline: true
+	});
+
+	// getNextEpisode(row);
+
+	let time_until = await getTimeUntil(nextAiring.airingAt);
+
+
+
+	fields.push({
+		name: 'Next Episode',
+		value: ''
+	});
+
+	const richEmbed = {
+		color: 0xaa98ae,
+		title: row.name,
+		description: `|| ${row.description} ||`,
+		fields,
+		timestamp: Date.now()
+	};
+
+	// message.channel.send({embed:richEmbed});
+}
+
+function getTimeUntil(timestamp){
+	return new Promise((resolve)=>{
+		const new_timestamp = timestamp * 1000;
+		console.log(new_timestamp);
+		let time = Date.now() - new_timestamp;
+		console.log(time);
+		let days = time / 86400000;
+		days = parseInt(days);
+		time = time - (days * 86400000);
+		let hours = time / 3600000;
+		hours = parseInt(hours);
+		time = time - (hours * 3600000);
+		let minutes = time / 60000;
+		minutes = parseInt(minutes);
+
+		console.log(`${days}D ${hours}H ${minutes}M`);
+
+		resolve(`${days}D ${hours}H ${minutes}M`);
+
 	});
 }
